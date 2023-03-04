54285c8b671eb4114b8482de3463195baec09b21
fgao63 <fgao63@gatech.edu>
2023-03-03 23:40:43 -0500

    Initial Commit

---
+++ NoteByNote/NoteByNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteByNoteApp.swift
+//  NoteByNote
+//
+//  Created by Frank Gao on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteByNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
